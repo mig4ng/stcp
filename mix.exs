9b9b47f6ac9081b6c23e43813f73a42ed369eaec
Fly Dev <dev@fly.io>
2025-07-19 02:12:10 +0000

    fix syntax error in deps list

--- mix.exs
+++ mix.exs
@@ -55,10 +55,10 @@ defmodule StcpStops.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      
-{:bandit, "~> 1.5"},,
-      
-{:floki, "~> 0.36"}
+
+      {:bandit, "~> 1.5"},
+
+      {:floki, "~> 0.36"}
     ]
   end
 
